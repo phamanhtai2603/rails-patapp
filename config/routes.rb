8a1500ec20b123aff8773aa3acfc49efc47b64ff
Anh Tai <phamanhtai263@gmail.com>
2019-11-26 11:29:51 +0700

    Chapter 3: Mostly static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root "static_pages#home" 
+  get  "/help", to: "static_pages#help"
+  get "/signup", to: "users#new"
+  resources :users
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  scope "(:locale)", locale: /en|vi/ do
+  	root "static_pages#home" 
+  end
 end
