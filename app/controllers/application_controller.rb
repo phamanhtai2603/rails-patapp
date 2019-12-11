37a7252e685a0b2b1691f6352aad5a776b40af51
Anh Tai <phamanhtai263@gmail.com>
2019-12-11 11:46:44 +0700

    Chapter 10: Updating, showing, and deleting users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
   include SessionsHelper
-  
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
 
   def set_locale
    locale = params[:locale].to_s.strip.to_sym
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  before_save   :downcase_email
+  before_create :create_activation_digest
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: Settings.maxlength }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   def remember
     self.remember_token = User.new_token
@@ -32,4 +34,16 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+
 end
