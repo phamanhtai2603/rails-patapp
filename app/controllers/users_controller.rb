dc70e9f6fa106a220cec08263d71f89305abd96d
Anh Tai <phamanhtai263@gmail.com>
2019-12-17 11:39:48 +0700

    Chapter 11: Account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,27 +4,28 @@ class UsersController < ApplicationController
   before_action :admin_user,  only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
-	def show
-		@user = User.find_by id: params[:id]
-	end
+  def show
+    @user = User.find_by id: params[:id]
+    redirect_to root_url and return unless true
+  end
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			log_in @user
-			flash[:success] = t "users.createsuccess"
-			redirect_to user_path(@user)
-		else
-		  render :new
-		end
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = t "users.pleasecheckactive"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
 
   def edit
     @user = User.find(params[:id])
